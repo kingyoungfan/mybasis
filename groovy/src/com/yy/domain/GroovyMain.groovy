7aad881ce206fe418e3a7d7989bfca548f5594fb
yangyang <yangyang@ixiye.com>
2016-07-27 18:19:38 +0800

    add thread and reconstruction

--- groovy/src/com/yy/domain/GroovyMain.groovy
+++ groovy/src/com/yy/domain/GroovyMain.groovy
@@ -1,10 +1,17 @@
 package com.yy.domain
 
+import groovy.transform.PackageScope
+
 /**
  * Created by yangyang on 16/7/22.
  */
 class GroovyMain {
+    @PackageScope String name;
+    String nickname;
     static void main(args){
+        GroovyMain groovyMain = new GroovyMain();
+        print groovyMain.name;
+        print("\n")
         println "hello world"
     }
 }
---
+++ thread/src/com/yy/test/Main.java
@@ -0,0 +1,16 @@
+package com.yy.test;
+
+/**
+ * Main
+ *
+ * @author yangyang
+ * @create 16/7/27 18:17
+ * @function
+ */
+public class Main {
+    public static void main(String[] args) {
+        MyThread myThread = new MyThread();
+        myThread.start();
+    }
+
+}
