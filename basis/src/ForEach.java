7b18c3b85574b18ad04ee4574736ed696d80da10
yangyang <yangyang@ixiye.com>
2016-07-29 10:36:31 +0800

    update

---
+++ basis/src/ForEach.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * ForEach
+ *
+ * @author yangyang
+ * @create 16/7/29 09:59
+ * @function
+ */
+public class ForEach {
+
+    public static void main(String[] args) {
+        System.out.println();
+        genList();
+    }
+
+    private static List<Map<Object, Object>> genList() {
+        List<Map<Object, Object>> ret = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            Map<Object, Object> map = new HashMap<>();
+            map.put("id", i);
+            map.put("name", "yangyang"+i);
+            System.out.println(map);
+
+            ret.add(map);
+            System.out.println(ret);
+        }
+        return ret;
+    }
+}
--- basis/src/net/Http.java
+++ basis/src/net/Http.java
@@ -1,5 +1,6 @@
 package net;
 
+
 import org.junit.Test;
 
 import java.io.BufferedReader;
