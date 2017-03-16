15d7907dbb25a74d62ef2d9368110450dbbf8dd8
yangyang <yangyang@ixiye.com>
2017-03-16 22:39:44 +0800

    duapte

--- mybatis/groovy/src/com/yy/domain/GroovyMain.groovy
+++ mybatis/groovy/src/com/yy/domain/GroovyMain.groovy
@@ -6,11 +6,13 @@ import groovy.transform.PackageScope
  * Created by yangyang on 16/7/22.
  */
 class GroovyMain {
-    @PackageScope String name;
+    @PackageScope
+    String name = "asas"
     String nickname;
-    static void main(args){
-        GroovyMain groovyMain = new GroovyMain();
-        print groovyMain.name;
+
+    static void main(args) {
+        GroovyMain groovyMain = new GroovyMain()
+        print groovyMain.name
         print("\n")
         println "hello world"
     }
